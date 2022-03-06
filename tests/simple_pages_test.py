3d87a332b5225bc15310cf4642d1e24b3f12a868
swapnilaryan97 <saiamthere@gmail.com>
2022-03-06 10:35:59 -0500

    added code snippet, git and merge content on page 1

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -14,7 +14,7 @@ def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Index Page" in response.data
+    assert b"Git" in response.data
 
 def test_request_about(client):
     """This makes the index page"""
@@ -26,7 +26,7 @@ def test_request_page1(client):
     """This makes the index page"""
     response = client.get("/page1")
     assert response.status_code == 200
-    assert b"Page 1" in response.data
+    assert b"Git" in response.data
 
 def test_request_page2(client):
     """This makes the index page"""
