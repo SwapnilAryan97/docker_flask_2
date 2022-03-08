b628786f375106ab4a544f5beea38cfcda84a160
swapnilaryan97 <saiamthere@gmail.com>
2022-03-07 20:02:58 -0500

    fixed bugs

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,23 +4,16 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">Page 1</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Page 2</a>' in response.data
-    assert b'<a class="nav-link" href="/page3">Page 3</a>' in response.data
-    assert b'<a class="nav-link" href="/page4">Page 4</a>' in response.data
+    assert b'<a class="nav-link" href="/page1">Git</a>' in response.data
+    assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
+    assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
+    assert b'<a class="nav-link" href="/page4">CI/CD</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
     assert b"Git" in response.data
-
-def test_request_about(client):
-    """This makes the index page"""
-    response = client.get("/about")
-    assert response.status_code == 200
-    assert b"About Page" in response.data
 
 def test_request_page1(client):
     """This makes the index page"""
