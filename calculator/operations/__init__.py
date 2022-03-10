243e8c3a47a9adda5a822f110bae8a1ccaf5cd6e
swapnilaryan97 <saiamthere@gmail.com>
2022-03-09 20:59:56 -0500

    added operations class for calculator

---
+++ calculator/operations/__init__.py
@@ -0,0 +1,30 @@
+class Addition:
+    """This is the Addition Class"""
+
+    def add(self, value1, value2):
+        """This is the addition method"""
+        return value1 + value2
+
+
+class Subtraction:
+    """This is the subtraction Class"""
+
+    def subtract(self, value1, value2):
+        """This is subtraction method"""
+        return value1 - value2
+
+
+class Multiplication:
+    """This is the Multiplication Class"""
+
+    def multiply(self, value1, value2):
+        """This is multiplication method"""
+        return value1 * value2
+
+
+class Divion:
+    """This is the Divion Class"""
+
+    def divide(self, value1, value2):
+        """This is division method"""
+        return value1 / value2
